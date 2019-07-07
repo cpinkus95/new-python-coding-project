3075f783243b5de21efdd0e91bb628f8c306d1ea
cpinkus95 <52015592+cpinkus95@users.noreply.github.com>
2019-07-07 17:46:36 -0400

    Calculator update

--- hello.py
+++ hello.py
@@ -1,5 +1,20 @@
 print("hello world")
+print("here is my calculator")
 
-print("how are you")
+num1 = float(input("Enter first number:"))
+op = input("Enter operation:")
+num2 = float(input("Enter second number:"))
+
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "/":
+    print(num1 / num2)
+elif op == "*":
+    print(num1 * num2)
+else:
+    print("Invalid operator")
+
 
 #these are for testing
